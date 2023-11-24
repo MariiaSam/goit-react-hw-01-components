f0e586b3b3c6cb1e420d89e6c127ea4fb2680000
MariiaSam <kontragent@ukr.net>
2023-11-24 13:52:58 +0200

    transition done

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,28 +1,29 @@
 import PropTypes from 'prop-types';
+import { Table, TableHead, TableRow, TableHeadItem, TableBody, TableBodyTr, TableBodyItem } from './TransactionHistory.styled'
 
 export const TransactionHistory = ({ items }) => {
   return (
-    <table class="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableHeadItem>Type</TableHeadItem>
+          <TableHeadItem>Amount</TableHeadItem>
+          <TableHeadItem>Currency</TableHeadItem>
+        </TableRow>
+      </TableHead>
 
-      <tbody>
+      <TableBody>
         {items.map(({ id, type, amount, currency }) => {
           return (
-            <tr key={id}>
-              <td>{type}</td>
-              <td>{amount}</td>
-              <td>{currency}</td>
-            </tr>
+            <TableBodyTr key={id}>
+              <TableBodyItem>{type}</TableBodyItem>
+              <TableBodyItem>{amount}</TableBodyItem>
+              <TableBodyItem>{currency}</TableBodyItem>
+            </TableBodyTr>
           );
         })}
-      </tbody>
-    </table>
+      </TableBody>
+    </Table>
   );
 };
 
---
+++ src/components/TransactionHistory/TransactionHistory.styled.jsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const Table = styled.table`
+  width: 400px;
+  margin-top: 50px;
+
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+
+  background-color: white;
+`;
+
+export const TableHead = styled.thead`
+  text-transform: uppercase;
+  text-shadow: 0.0625rem 0.0625rem 0.0625rem #fff;
+
+  background-color: rgb(131, 203, 197);
+  border-radius: 10px;
+`;
+
+export const TableRow = styled.tr`
+  text-align: center;
+`;
+
+export const TableHeadItem = styled.th`
+  color: white;
+
+  font-size: 24px;
+  font-weight: 400;
+  border: 2px solid rgb(3, 52, 48);
+  padding: 3px 2px;
+`;
+
+export const TableBody = styled.tbody`
+font-size: 18px;
+  text-align: center;
+` 
+
+export const TableBodyTr = styled.tr`
+  background-color: rgb(139, 187, 183);
+  `;
+
+  export const TableBodyItem = styled.td`
+ padding: 8px 0;
+
+  `
