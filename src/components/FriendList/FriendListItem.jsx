a3df039bbf87a40417b43980fde7da111d27a1a3
MariiaSam <kontragent@ukr.net>
2023-11-25 11:49:51 +0200

    corr

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 import { Item, Status, Image, Name } from './FriendListItem.styled';
 
-export const FriendListItem = ({ avatar, name, isOnline, Names }) => {
+export const FriendListItem = ({ avatar, name, isOnline}) => {
   return (
-    <Item>
-      <Status isOnline></Status>
+    <Item >
+      <Status isOnline={isOnline}></Status>
       <Image src={avatar} alt="User avatar" width="48" />
       <Name>{name}</Name>
     </Item>
@@ -12,7 +12,7 @@ export const FriendListItem = ({ avatar, name, isOnline, Names }) => {
 };
 
 FriendListItem.propTypes = {
+  isOnline: PropTypes.bool.isRequired,
   avatar: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
 };
